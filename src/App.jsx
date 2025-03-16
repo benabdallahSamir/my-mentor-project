b82981fa3ca7413dcc65c4d2e88ace4ee947dfe8
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-03-16 01:44:19 +0100

    change routes structur and color palet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import setTheme from "./controllers/them";
 import HomeContianer from "./pages/HomeContianer";
-import Home from "./pages/Home";
 import ThemeButton from "./pages/components/ThemeButton";
 
 function App() {
   setTheme();
   return (
-    <div className="dark:bg-background-dark dark:text-text-dark bg-background-light text-text-light min-w-screen min-h-screen h-max w-max overflow-x-hidden relative">
+    <div className="dark:bg-background-dark dark:text-text-dark bg-background-light text-text-light min-w-screen min-h-screen h-screen w-max overflow-x-hidden relative">
       <ThemeButton />
       <Routes>
-        <Route path="/" element={<HomeContianer />}>
-          <Route index element={<Home />} />
-        </Route>
+        <Route path="/" element={<HomeContianer />} />
       </Routes>
     </div>
   );
