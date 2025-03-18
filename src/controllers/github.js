4ca8c996b8e0d9a084593266c4dc0e8354f91875
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-03-19 00:09:33 +0100

    fix bug (limit the number of repo)

--- src/controllers/github.js
+++ src/controllers/github.js
@@ -5,8 +5,17 @@ const GITHUB_API_URL = "https://api.github.com";
 const fetchUserRepositories = async () => {
   try {
     const response = await axios.get(
-      `${GITHUB_API_URL}/users/samirBenabdallah/repos`
+      `${GITHUB_API_URL}/users/samirBenabdallah/repos`,
+      {
+        params: {
+          page: 1,
+          per_page: 200, // GitHub's default limit per page
+        },
+      }
     );
+    // const response = await axios.get(
+    //   `${GITHUB_API_URL}/users/samirBenabdallah/repos`
+    // );
     const repositories = [];
     response.data.map((repo) => {
       const r = handleReposetry(repo);
@@ -18,10 +27,12 @@ const fetchUserRepositories = async () => {
     throw error;
   }
 };
-
 function handleReposetry(repo) {
   const { homepage: livesite, name, html_url: link, description } = repo;
-  if (!description) return;
+  if (!description) {
+    console.log(link);
+    return;
+  }
   const image = `${link}/blob/main/design/desktop-preview.jpg?raw=true`;
   const level = description.match(/: [A-Za-z]*/)[0].substring(2);
   const language = description.match(/- .*/)[0].substring(2);
@@ -34,5 +45,4 @@ function handleReposetry(repo) {
     language,
   };
 }
-
 export default fetchUserRepositories;
